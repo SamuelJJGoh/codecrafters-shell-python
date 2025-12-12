cdd8bcbdc439f377e9444ef151d9c69bc214c9a1
Samuel Goh Jun Je <samueljjgoh@gmail.com>
2025-12-12 19:51:22 +0000

    Run a program

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import subprocess
 
 def main():
 
@@ -13,8 +14,10 @@ def main():
 
         if command.startswith("type"):
             check_command = command.replace("type", "", 1).strip()
-    
-            if check_command in built_in_commands :
+            if check_command == "":
+                main()
+                break
+            elif check_command in built_in_commands :
                 print(f"{check_command} is a shell builtin")
             else :
                 all_paths = os.environ["PATH"]
@@ -30,13 +33,24 @@ def main():
             main()
             break
         elif command == "exit":
-            break
+            break            
         elif "echo" in command:
             output = command.replace("echo", "").strip()
             print(output)
             main()
             break
-        
+        else : 
+            program_name = command.split(" ")[0]
+            args = command.split(" ")[1:]
+            all_paths = os.environ["PATH"]
+            directories = all_paths.split(":")
+            for directory in directories :
+                potential_executable = directory + "/" + program_name
+                if os.path.isfile(potential_executable) and os.access(potential_executable, os.X_OK) :
+                    subprocess.call([program_name, *args])
+                    main()
+                else : 
+                    continue
         print(f"{command}: command not found")
 
         
