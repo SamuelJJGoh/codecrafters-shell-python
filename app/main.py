5d67b4178f1e68c3dbfc819e54cc22a2c63e8a8a
Samuel Goh Jun Je <samueljjgoh@gmail.com>
2025-12-12 16:04:16 +0000

    Implement echo

--- app/main.py
+++ app/main.py
@@ -6,13 +6,21 @@ def main():
 
     while True:
         sys.stdout.write("$ ")
+
         # Wait for user input
         command = input()
 
         if command == "exit":
             break
-
+        elif "echo" in command:
+            output = command.replace("echo", "").strip()
+            print(output)
+            main()
+            break
+        
         print(f"{command}: command not found")
+
+        
 
 
 
